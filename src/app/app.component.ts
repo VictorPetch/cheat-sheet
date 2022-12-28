8cd18ee2f86584a62b9b6a3df0d5e9514e187750
VictorPetch <victorpetch06@gmail.com>
2022-12-28 02:50:10 -0300

    Added route section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cheat-sheet';
+	title = 'cheat-sheet';
 
-  text = 
-`{
-	"projects": {
-		"<project-name>": {
-			"architect": {
-				"build":{
-					"options" : {
-						"outputPath": "docs",
-					}
-				}
-			}
-		} 
-	}
-}`
-
-text2 = `ng build --output-path docs --base-href /<project-name>/`
-
-textDict = {
-	'deploy1': `ng build --output-path docs --base-href /<project-name>/`,
-	'deploy2': `{
+	textDict = {
+		'deploy1': `ng build --output-path docs --base-href /<project-name>/`,
+		'deploy2': `{
 	"projects": {
 		"<project-name>": {
 			"architect": {
@@ -39,14 +22,14 @@ textDict = {
 			}
 		} 
 	}\n}`,
-	'deploy3': `{
+		'deploy3': `{
 	"projects": {
 		"<project-name>"
 	}\n}`,
-	'image1': `index.html\nassets
+		'image1': `index.html\nassets
 	images
 		image1.png`,
-	'image2': `<img src="assets/images/deploy2.png">`,
-		
-  };
+		'image2': `<img src="assets/images/deploy2.png">`,
+
+	};
 }
