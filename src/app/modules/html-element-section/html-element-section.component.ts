294f162c47efe740937f539aa7ff094b32d3d099
VictorPetch <victorpetch06@gmail.com>
2023-04-19 22:37:43 -0300

    Added html element section

---
+++ src/app/modules/html-element-section/html-element-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html-element-section',
+  templateUrl: './html-element-section.component.html',
+  styleUrls: ['./html-element-section.component.scss']
+})
+export class HtmlElementSectionComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+
+}
